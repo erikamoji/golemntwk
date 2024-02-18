76b726330adddb7a7f4bbe3b808a76bbf34e0e11
erikamoji <ekpnguyen@gmail.com>
2024-02-18 13:00:33 +0700

    changes in folder

--- reputation_listener/listener.js
+++ reputation_listener/listener.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Web3 = require('web3');
-const ReputationABI = require('./abis/Reputation.json'); // Assuming this is the correct path
+const ReputationABI = require('./Reputation.json'); // Assuming this is the correct path
 
 const web3 = new Web3(process.env.WEB3_PROVIDER_URL);
 
