297410233c2cebd687f4557157fc19f7b8458d2e
erikamoji <ekpnguyen@gmail.com>
2024-02-18 17:13:54 +0700

    golem test

---
+++ try_golem/requestor.mjs
@@ -0,0 +1,16 @@
+import { TaskExecutor } from "@golem-sdk/golem-js";
+
+(async () => {
+  const executor = await TaskExecutor.create({
+    package: "529f7fdaf1cf46ce3126eb6bbcd3b213c314fe8fe884914f5d1106d4",
+    yagnaOptions: { apiKey: "e6d765724c434d838bd793e32d04865a" },
+  });
+  try {
+    const result = await executor.run(async (ctx) => (await ctx.run("node -v")).stdout);
+    console.log("Task result:", result);
+  } catch (err) {
+    console.error("An error occurred:", err);
+  } finally {
+    await executor.shutdown();
+  }
+})();
